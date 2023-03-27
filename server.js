bb0e30004cc0a4af2d06316849006c8b0bc69720
sam <subterranean.layer@gmail.com>
2023-03-27 12:16:03 -0400

    bootcamp model and CRUD controllers added

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ dotenv.config({
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV == 'development') {
 	app.use(morgan('dev'));
